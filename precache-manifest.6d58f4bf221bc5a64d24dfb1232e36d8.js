35e183b87921d9227c5e2140ef696c38748d4aba
Taylor Thibodeaux <taythib@gmail.com>
2020-01-13 12:16:15 -0600

    Updates

---
+++ precache-manifest.6d58f4bf221bc5a64d24dfb1232e36d8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0d85e8273261e18e60ba7b1ff743ccf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b28ac640cf43a6aa0ec",
+    "url": "/static/css/main.ad439f63.chunk.css"
+  },
+  {
+    "revision": "629e593d9d58eb3b2a18",
+    "url": "/static/js/2.625582d7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.625582d7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b28ac640cf43a6aa0ec",
+    "url": "/static/js/main.d6dc32b9.chunk.js"
+  },
+  {
+    "revision": "830df273c0a45ed111b3",
+    "url": "/static/js/runtime-main.72091a38.js"
+  },
+  {
+    "revision": "42dcd8191ebf8e295aedb93a679e2c1f",
+    "url": "/static/media/blurb.42dcd819.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0f968dd5fcaad5ca223bbede01cf2ab6",
+    "url": "/static/media/shotgun.0f968dd5.png"
+  }
+]);
