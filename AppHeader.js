ae3fc20bc28e0d30db7f583b132d6721e7773227
Taylor Thibodeaux <taythib@gmail.com>
2023-02-27 08:46:19 -0600

    Updates

---
+++ AppHeader.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function AppHeader() {
+  return (
+      <header className="App-header">
+        <div className="Title">
+          Taylor Thibodeaux
+        </div>
+        <div className="Links">
+          <a>
+            About
+          </a>
+          <a>
+            Projects
+          </a>
+          <a>
+            Resume
+          </a>
+          <a>
+            Contact
+          </a>
+        </div>
+      </header>
+  );
+}
+
+export default AppHeader;
---
+++ precache-manifest.ed4d1efb5d66a4c654b210d5a015a4c2.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b80763f98a9eac45e2e72add6eebd336",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca326fe4406732c0d761",
+    "url": "/static/css/main.94ff2cb6.chunk.css"
+  },
+  {
+    "revision": "b1c6abe99e3d3d89cf64",
+    "url": "/static/js/2.08d682fd.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.08d682fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ca326fe4406732c0d761",
+    "url": "/static/js/main.7faf738e.chunk.js"
+  },
+  {
+    "revision": "0dda34acc7b81aad8062",
+    "url": "/static/js/runtime-main.c399816e.js"
+  },
+  {
+    "revision": "fb76b22a7b6e672441d8d0d7f4980c38",
+    "url": "/static/media/Screenshot.fb76b22a.png"
+  },
+  {
+    "revision": "ee586586593313c3c814800033b5efc3",
+    "url": "/static/media/agreements.ee586586.png"
+  },
+  {
+    "revision": "50820a94ce9c00a0cae5b722d3b6c986",
+    "url": "/static/media/alignpriorities.50820a94.png"
+  },
+  {
+    "revision": "98d36fc9635f9c1e04d96be5aa2846a9",
+    "url": "/static/media/blurb.98d36fc9.svg"
+  },
+  {
+    "revision": "ecd4712a7f40eea33f35434c3dc3822d",
+    "url": "/static/media/boston.ecd4712a.jpg"
+  },
+  {
+    "revision": "4785d6fe9a2134ab953343d000267e0d",
+    "url": "/static/media/cat-tile.4785d6fe.svg"
+  },
+  {
+    "revision": "74972c1b9bfb5e303e902a0443bd2064",
+    "url": "/static/media/cloud.74972c1b.svg"
+  },
+  {
+    "revision": "f1016723c7af1d8a65577f3b6b1392c9",
+    "url": "/static/media/dashboard.f1016723.png"
+  },
+  {
+    "revision": "59e3fc2e597c1c597a782942aa04c63a",
+    "url": "/static/media/full-image.59e3fc2e.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "b57cc18979abba59649bde2129d7d0f1",
+    "url": "/static/media/machinery.b57cc189.jpg"
+  },
+  {
+    "revision": "200e9127b4b9b7378cbf68e001160e15",
+    "url": "/static/media/mockup.200e9127.png"
+  },
+  {
+    "revision": "e518272012321768d6dd700d863c0e6f",
+    "url": "/static/media/payment-methods.e5182720.png"
+  },
+  {
+    "revision": "52a94934e093d1e29779bc579e5798b7",
+    "url": "/static/media/plant.52a94934.jpg"
+  },
+  {
+    "revision": "ea5866bae336c540c64cfa8c8f73ea5a",
+    "url": "/static/media/plant2.ea5866ba.jpeg"
+  },
+  {
+    "revision": "1eaee925a5eb38369165f3b9d449afd5",
+    "url": "/static/media/security.1eaee925.jpg"
+  },
+  {
+    "revision": "0f968dd5fcaad5ca223bbede01cf2ab6",
+    "url": "/static/media/shotgun.0f968dd5.png"
+  }
+]);
